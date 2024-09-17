a501b5d1bc58c5e71acf72dbfd943b79084fc748
AkosiBatm4n <yanyan.another.acc0unt@gmail.com>
2024-09-16 19:09:28 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+const App = () => {
+
+  return <section className="grid gap-8 sm:max-xl:bg-blue-50 sm:max-xl:p-9 md:grid-cols-2 md: items-center md:text-left">
+ <div>
+
+<img 
+src="https://th.bing.com/th/id/R.c178dc0260938c892794e79dc9a2d31a?rik=BG9EcEVqdfRjag&riu=http%3a%2f%2fstatic.independent.co.uk%2fs3fs-public%2fstyles%2fstory_large%2fpublic%2fthumbnails%2fimage%2f2015%2f07%2f25%2f20%2f1-concorde-get.jpg&ehk=j9U8dLMXo9V99e0HrOtDyNi%2fBXoVxh2evJHs5lBOEA0%3d&risl=&pid=ImgRaw&r=0"alt=""
+className="w--full rounded-lg"
+/>
+
+ </div>
+<div>
+  <h1 className="text-4xl font medium mb-2">Aircraft File</h1>
+  <p>Concorde (/ˈkɒŋkɔːrd/) is a retired Anglo-French supersonic airliner jointly developed and manufactured by Sud Aviation (later Aérospatiale) and the British Aircraft Corporation (BAC). Studies started in 1954, and France and the United Kingdom signed a treaty establishing the development project on 29 November 1962, as the programme cost was estimated at £70 million (£1.68 billion in 2023). Construction of the six prototypes began in February 1965, and the first flight took off from Toulouse on 2 March 1969. The market was predicted for 350 aircraft, and the manufacturers received up to 100 option orders from many major airlines. On 9 October 1975, it received its French Certificate of Airworthiness, and from the UK CAA on 5 December.[4]
+
+Concorde is a tailless aircraft design with a narrow fuselage permitting 4-abreast seating for 92 to 128 passengers, an ogival delta wing and a droop nose for landing visibility. It is powered by four Rolls-Royce/Snecma Olympus 593 turbojets with variable engine intake ramps, and reheat for take-off and acceleration to supersonic speed. Constructed out of aluminium, it was the first airliner to have analogue fly-by-wire flight controls. The airliner could maintain a supercruise up to Mach 2.04 (2,170 km/h; 1,350 mph) at an altitude of 60,000 ft (18.3 km) WHICH FLIES AT THE COST OF YOUR ENTIRE HOUSE.</p>
+</div>
+
+  </section>;
+};
+
+export default App;
